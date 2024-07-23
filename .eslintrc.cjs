e897a41ef89e7eda3cad59196e3d5f1005e3a7d6
liuleihang <474733548@qq.com>
2024-07-23 16:48:26 +0800

    chore: eslint配置修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,28 +6,22 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true
+    es6: true,
   },
   extends: [
     // vue 规则
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     // 告诉 ESLint 关闭与 Prettier 格式化规则冲突的任何规则，需写在最后，会覆盖前面的配置
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   // 添加 vue文件解析器  解析template文件
   parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    // 告诉 ESLint 关闭与 Prettier 格式化规则冲突的任何规则，需写在最后，会覆盖前面的配置
-    'plugin:prettier/recommended'
-  ],
   rules: {
     // 强制数组元素间出现换行
     'array-element-newline': 0,
@@ -41,13 +35,13 @@ module.exports = {
       'error',
       {
         singleline: 'ignore',
-        multiline: 'ignore'
-      }
+        multiline: 'ignore',
+      },
     ],
     // 忽略组件命名为多词限制
     'vue/multi-word-component-names': 'off',
     // 忽略未使用变量
     '@typescript-eslint/no-unused-vars': 'off',
-    'no-unused-vars': 'off'
-  }
+    'no-unused-vars': 'off',
+  },
 };
